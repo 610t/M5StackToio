e716874446ca8e9b038933b750efd12ba36397ff
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2024-11-28 16:13:38 +0900

    Support Faces Joystick. Now don't work for Joystick Unit.

--- examples/joystick_drive/joystick_drive.ino
+++ examples/joystick_drive/joystick_drive.ino
@@ -38,10 +38,7 @@
 #include <Wire.h>
 #include <Toio.h>
 
-static const uint16_t I2C_FREQ = 400000;
-static const uint8_t JOYSTICK_I2C_ADDR = 0x52;
-static const uint8_t JOYSTICK_GPIO_SDA = 21;
-static const uint8_t JOYSTICK_GPIO_SCL = 22;
+static const uint8_t JOYSTICK_I2C_ADDR = 0x5e;
 
 // ライト ON/OFF 状態
 static bool light_on = false;
@@ -56,21 +53,21 @@ static uint8_t carib_y = 130;
 // MIDI データ (チャルメラ)
 static const uint8_t CHARUMERA_LEN = 39;
 static uint8_t CHARUMERA_DATA[CHARUMERA_LEN] = {
-  3,             // Type of Sound control (MIDI)
-  1,             // Repeat count
-  12,            // Number of operations
-  14,  69, 255,  // 140ms, A5
-  14,  71, 255,  // 140ms, B5
-  56,  73, 255,  // 560ms, C#6
-  14,  71, 255,  // 140ms, B5
-  14,  69, 255,  // 140ms, A5
-  114, 128, 255, // no sound
-  14,  69, 255,  // 140ms, A5
-  14,  71, 255,  // 140ms, B5
-  14,  73, 255,  // 560ms, C#6
-  14,  71, 255,  // 140ms, B5
-  14,  69, 255,  // 140ms, A5
-  56,  71, 255   // 560ms, B5
+  3,              // Type of Sound control (MIDI)
+  1,              // Repeat count
+  12,             // Number of operations
+  14, 69, 255,    // 140ms, A5
+  14, 71, 255,    // 140ms, B5
+  56, 73, 255,    // 560ms, C#6
+  14, 71, 255,    // 140ms, B5
+  14, 69, 255,    // 140ms, A5
+  114, 128, 255,  // no sound
+  14, 69, 255,    // 140ms, A5
+  14, 71, 255,    // 140ms, B5
+  14, 73, 255,    // 560ms, C#6
+  14, 71, 255,    // 140ms, B5
+  14, 69, 255,    // 140ms, A5
+  56, 71, 255     // 560ms, B5
 };
 
 void displayCaptionButtonA(String caption) {
@@ -90,9 +87,9 @@ void displayCaptionButtonC(String caption) {
 
 void drawJoystick(float jx, float jy) {
   M5.Lcd.setCursor(70, 130, 2);
-  M5.Lcd.printf("X : %d  ", (int)jx);
+  M5.Lcd.printf("X : %f  ", jx);
   M5.Lcd.setCursor(70, 150, 2);
-  M5.Lcd.printf("Y : %d  ", (int)jy);
+  M5.Lcd.printf("Y : %f  ", jy);
   M5.Lcd.drawCircle(190, 150, 50, 0xffffff);
   M5.Lcd.fillCircle(190, 150, 49, 0x000000);
   M5.Lcd.drawLine(140, 150, 240, 150, 0xffffff);
@@ -119,7 +116,7 @@ void setup() {
   M5.Lcd.print("M5StackToio");
 
   // ジョイスティック利用を開始
-  Wire.begin(JOYSTICK_GPIO_SDA, JOYSTICK_GPIO_SCL, I2C_FREQ);
+  Wire.begin();
 
   // 3 秒間 Toio Core Cube をスキャン
   M5.Lcd.setCursor(0, 50, 2);
@@ -156,7 +153,7 @@ void loop() {
   M5.update();
 
   // ジョイスティックが接続されているかをチェック
-  Wire.requestFrom(JOYSTICK_I2C_ADDR, 3);
+  Wire.requestFrom(JOYSTICK_I2C_ADDR, 5);
   if (!Wire.available()) {
     return;
   }
@@ -166,9 +163,15 @@ void loop() {
   toio.loop();
 
   // ジョイスティックの状態を取得
-  uint8_t x = Wire.read();
-  uint8_t y = Wire.read();
+  uint8_t adX_L = Wire.read();
+  uint8_t adX_H = Wire.read();
+  uint8_t adY_L = Wire.read();
+  uint8_t adY_H = Wire.read();
   uint8_t z = Wire.read();
+
+  float y = map((float)(adX_H << 8 | adX
+  _L), 250.0, 800.0, -100.0, 100.0) + 130.0;
+  float x = map((float)(adY_H << 8 | adY_L), 250.0, 800.0, -100.0, 100.0) + 130.0;
 
   // M5Stack のボタン C が押されたときの処理 (Carib)
   if (M5.BtnC.wasPressed()) {
@@ -178,8 +181,8 @@ void loop() {
   }
 
   // ジョイスティックの x/y 軸の位置を特定 (キャリブレーション済み)
-  float jy = ((float)y - (float)carib_y) * 100.0 / 120.0;
-  float jx = ((float)x - (float)carib_x) * 100.0 / 120.0;
+  float jy = ((float)y - (float)carib_y);
+  float jx = ((float)x - (float)carib_x);
   float jy_sign = (jy < 0) ? -1 : 1;
 
   // ジョイスティックの状態を画面に表示
@@ -194,9 +197,9 @@ void loop() {
   if (M5.BtnA.wasReleased()) {
     // Toio Core Cube と BLE 接続中かどうかをチェック
     if (toiocore->isConnected()) {
-      toiocore->disconnect(); // 接続中なら切断
+      toiocore->disconnect();  // 接続中なら切断
     } else {
-      toiocore->connect(); // 切断中なら接続
+      toiocore->connect();  // 切断中なら接続
     }
     return;
   }
@@ -209,9 +212,9 @@ void loop() {
   // M5Stack のボタン B が押されたときの処理 (Light)
   if (M5.BtnB.wasPressed()) {
     if (light_on) {
-      toiocore->turnOffLed(); // 点灯中なら消灯
+      toiocore->turnOffLed();  // 点灯中なら消灯
     } else {
-      toiocore->turnOnLed(255, 255, 255); // 消灯中なら白で点灯
+      toiocore->turnOnLed(255, 255, 255);  // 消灯中なら白で点灯
     }
     light_on = !light_on;
   }
@@ -236,7 +239,7 @@ void loop() {
   throttle = (float)throttle * 2 / 3;
 
   // ステアリング
-  int8_t steering = (abs(jx) < 10) ? 0 : jx; // 10 ほど遊びを入れる
+  int8_t steering = (abs(jx) < 10) ? 0 : jx;  // 10 ほど遊びを入れる
   if (steering > 100) {
     steering = 100;
   }
